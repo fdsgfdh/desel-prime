ace3dbf240c9c66bcd5d5f0428e308799799794a
fdsgfdh <robinsonjuan79@yahoo.com>
2024-02-29 14:57:22 +1100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+require('dotenv').config();
+
+// Function to check if a number is prime
+function isPrime(num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+
+    if (num % 2 === 0 || num % 3 === 0) return false;
+
+    let i = 5;
+    while (i * i <= num) {
+        if (num % i === 0 || num % (i + 2) === 0) return false;
+        i += 6;
+    }
+
+    return true;
+}
+
+// Function to generate prime numbers up to a given limit
+function generatePrimes(limit) {
+    const primes = [];
+    for (let i = 2; i <= limit; i++) {
+        if (isPrime(i)) {
+            primes.push(i);
+        }
+    }
+    return primes;
+}
+
+module.exports = generatePrimes;
